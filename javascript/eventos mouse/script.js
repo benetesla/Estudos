d339718f68f46a36308fdcd2a781c0391fd94a48
benetesla <78994881+benetesla@users.noreply.github.com>
2022-04-17 18:13:39 -0300

    Estudos
    #estudos Javascript

---
+++ javascript/eventos mouse/script.js
@@ -0,0 +1,9 @@
+function acao1(params) {
+    alert('Corre que aqui e o Rio de Janeiro')
+
+}
+
+function acao2(params) {
+    alert('Ta vendo eu avisei pra correr')
+
+}
---
+++ javascript/eventos.teclado/script.js
@@ -0,0 +1,3 @@
+function acao1(params) {
+    alert('usuario burro')
+}
